0fe58465a0d236ebe63453714facda42020658e4
GooKu <kuowods@hotmail.com>
2020-02-01 19:05:30 +0800

    Implement ShowWinner and ShowTie.

--- Assets/Scripts/UI/EndUI.cs
+++ Assets/Scripts/UI/EndUI.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EndUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private GameObject winLogo = null;
+    [SerializeField]
+    private Image winnerImg = null;
+    [SerializeField]
+    private Sprite redGroupSprite = null;
+    [SerializeField]
+    private Sprite blueGroupSprite = null;
+    [SerializeField]
+    private Sprite tieSprite = null;
+
+    public void ShowWinner(GroupType winner)
     {
-        
+        winLogo.SetActive(true);
+        winnerImg.sprite = winner == GroupType.Blue ? blueGroupSprite : redGroupSprite;
+        gameObject.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ShowTie()
     {
-        
+        winLogo.SetActive(false);
+        winnerImg.sprite = tieSprite;
+        gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
     }
 }
