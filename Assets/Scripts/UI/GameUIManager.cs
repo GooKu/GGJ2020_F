2c6ad91f37ed03ea5be1d58807d617c51305b8bd
GooKu <kuowods@hotmail.com>
2020-02-01 19:51:45 +0800

    Update path of GameUIManager and fix meta issue.

---
+++ Assets/Scripts/UI/GameUIManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameUIManager : MonoBehaviour
+{
+    [Header("TeamA分數")]
+    public Text TeamAScoreText;
+    [Header("TeamB分數")]
+    public Text TeamBScoreText;
+    int TeamAScore = 0;
+    int TeamBScore = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void setTeamAScore(int score)
+    {
+        this.TeamAScore = score;
+        TeamAScoreText.text = score.ToString();
+    }
+    public int getTeamAScore()
+    {
+        return this.TeamAScore;
+    }
+    public void setTeamBScore(int score)
+    {
+        this.TeamBScore = score;
+        TeamBScoreText.text = score.ToString();
+    }
+    public int getTeambScore()
+    {
+        return this.TeamBScore;
+    }
+
+}
