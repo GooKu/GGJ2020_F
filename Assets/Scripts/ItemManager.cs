7f8e4ecccd8c6f4febe6a93cd9f6bbac0b1ba78e
winniechiao <suger820807@gmail.com>
2020-02-01 16:58:33 +0800

    Add Random Create Function.

--- Assets/Scripts/ItemManager.cs
+++ Assets/Scripts/ItemManager.cs
@@ -6,22 +6,31 @@ public class ItemManager : MonoBehaviour
 {
     public MaskType maskType;
 
+    [Header("初始Rotation")]
+    public Vector3 defaultRotation;
+
+    [Header("在地板上的Y值")]
+    public float yGroundPos;
+    [Header("落下速度")]
+    public float fallSpeed;
+
     bool canCatch = true;
+    
     // Start is called before the first frame update
     void Start()
     {
-
+        transform.rotation = Quaternion.Euler(defaultRotation.x,Random.Range(0,360),defaultRotation.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, yGroundPos, transform.position.z), fallSpeed * Time.deltaTime);
     }
     
 
     private void OnTriggerEnter(Collider other)
-    {                
+    {      
         if (canCatch)
         {
             if (other.tag == TagManager.Player)
